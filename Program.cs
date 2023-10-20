6551d17e3ad78d5bcf8d1dae073816820c9937e8
Gabriel Jirjis <107893028+Jirjis77@users.noreply.github.com>
2023-10-20 17:55:17 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,34 @@
-﻿namespace roulette.cs;
+using System;
+using static System.Console;
 
-class Program
+// Totala Saldo att börja med
+int balance = 1000;
+// Välkomnar till spelet 
+WriteLine("Välkommen till Grupp 2 Roulett-spel!");
+//Förklarar nuvarande saldo 
+WriteLine($"Ditt nuvarande Saldo är: ${balance}");
+
+// om saldot är över $0 ber consolen om ett nummer mellan 0-36
+while (balance > 0)
 {
-    static void Main(string[] args)
+    Write("Vänligen välj ett nummer mellan 0 och 36: ");
+    int selectedNumber = int.Parse(ReadLine());
+    
+    // Om nummer är minder än noll alltså -1 eller högre än 36 säger consolen ifrån 
+    if (selectedNumber < 0 || selectedNumber > 36)§
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
+        WriteLine("Felaktigt nummer. Välj ett nummer mellan 0 och 36.");
+        continue;
     }
+    // Consolen ber spelaren om hur mycket man vill satsa
+    Write("Placera din satsning: $");
+    int betAmount = int.Parse(ReadLine());
+    // om du inte har tillräckligt med penagar komemr consolen att säga ifrån och inte låta dig spela
+    if (betAmount > balance)
+    {
+        WriteLine("Tyvärr! du har inte tillräckligt med saldo för denna satsning.");
+        continue;
+    }
+
+    
 }
-
-
-HEJ
